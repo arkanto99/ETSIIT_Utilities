0b6cebc6176b02fee70b06b9997fcc3365467e03
Sergio Jerez <sj.jerez@gmail.com>
2021-11-07 19:53:31 +0100

    multitouch 1

--- app/src/main/java/com/NPI/etsiitutilities/ActivityComedor.java
+++ app/src/main/java/com/NPI/etsiitutilities/ActivityComedor.java
@@ -3,12 +3,58 @@ package com.NPI.etsiitutilities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ActivityComedor extends AppCompatActivity {
 
+    // Elementos que necesitamos
+    // --------------------------------------------------------
+    private ImageView box_lunes, plato_l_1;
+
+
+
+
+    // Generación de la vista
+    // --------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Creación
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comedor);
+
+        // Capturamos los elementos
+        box_lunes = findViewById(R.id.box_lunes);
+        plato_l_1 = findViewById(R.id.plato_l_1);
+
+
+
+
+
+
+        // Aplicamos eventos necesarios a nuestros elementos
+        box_lunes.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int dedos=event.getPointerCount();
+
+                if (dedos==2){
+                        // Si se ha pulsado con un solo dedo, se mostrará el primer plato
+                        plato_l_1.setAlpha((float) 1);
+                }else{
+                    plato_l_1.setAlpha((float) 0);
+                }
+
+                return true;
+            }
+
+
+        });
     }
+
+
+
 }
