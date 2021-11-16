c71dd7cb1774f12776bd2b78c238262f630aeebc
Pablo <pablo.rodriguez.fernandez0@rai.usc.es>
2021-11-16 18:59:58 +0100

    Actualiza versión Base de Datos

--- app/src/main/java/data/IndicacionesDbHelper.java
+++ app/src/main/java/data/IndicacionesDbHelper.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 
 public class IndicacionesDbHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 5;
+    public static final int DATABASE_VERSION = 6;
     public static final String DATABASE_NAME = "BaseDatos.db";
 
     public IndicacionesDbHelper(Context context) {
